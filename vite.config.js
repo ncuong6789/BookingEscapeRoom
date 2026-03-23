041d56371b5bf4e46fea20c5edea899a3354bbb8
Nguyễn Lê Hùng Cường <nhokghoul69@gmail.com>
2026-03-23 12:16:52 +0700

    up page

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+// Thay 'TEN_REPO_CUA_BAN' bằng tên repository của bạn trên GitHub
+// Ví dụ: nếu link là https://github.com/ncuong6789/BookingEscapeRoom thì thay bằng '/BookingEscapeRoom/'
+export default defineConfig({
+  base: '/TEN_REPO_CUA_BAN/', 
+})
